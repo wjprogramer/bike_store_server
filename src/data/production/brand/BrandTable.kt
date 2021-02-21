ef0a054501cd866115d5913ef7db3a232a87c78e
jay_wu <jay_wu>
2021-02-21 21:08:59 +0800

    🐛 BrandTable cant be created by SchemaUtils (maybe problem of name)

--- src/data/production/brand/BrandTable.kt
+++ src/data/production/brand/BrandTable.kt
@@ -2,6 +2,9 @@ package com.giant_giraffe.data.production.brand
 
 import org.jetbrains.exposed.dao.IntIdTable
 
-object BrandTable: IntIdTable("brands") {
+/**
+ * `name`: 取名為 `brands` 時，`SchemaUtils.create` 會失敗
+ */
+object BrandTable: IntIdTable("brand") {
     val name = varchar("name", 255)
 }
