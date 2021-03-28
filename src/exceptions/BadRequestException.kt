3983b10179d20d43f2065665b167d38bb52e980a
jay_wu <jay_wu>
2021-03-28 15:55:37 +0800

    🩹 change bad request error code

--- src/exceptions/BadRequestException.kt
+++ src/exceptions/BadRequestException.kt
@@ -3,6 +3,6 @@ package com.giant_giraffe.exceptions
 class BadRequestException (
     message: String? = null,
 ): MyBaseException(
-    errorCode = 4030,
+    errorCode = 4000,
     message = message,
 )
